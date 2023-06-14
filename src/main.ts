a4d8490d6a2258c7a9e80299883b4d68834bb141
huangming <ming_5280@163.com>
2023-06-14 19:17:36 +0800

    ✨ feat: 增加图标

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import setupComponents from '/@/plugins/element';
 
 import '/@/styles/index.scss';
 import '/@/styles/var/element-theme.scss';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 // 创建vue实例
 const app = createApp(App);
@@ -20,6 +21,11 @@ app.use(router);
 
 // 路由守卫控制
 // import './router/permission'
+
+// 全局注册 ElementPlus Icon
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 // Register ElementPlus UI components
 setupComponents(app);
